6aff44351fc5def535c8956bddc24a96201519f0
PluieYu <yuyoubiyi@hotmail.com>
2024-03-25 09:15:29 +0100

    init

---
+++ Locale-zhCN.lua
@@ -0,0 +1,10 @@
+
+local L = AceLibrary("AceLocale-2.2"):new("MasterLoot")
+L:RegisterTranslations("zhCN", function() return {
+	["小皮箱队长分配助手"] = "|cffFF6060[小皮箱]|r|cff9482C9团队分配助手|r",
+	["分给自己"] = "分给自己",
+	["全团随机roll"] ="全团随机roll",
+	["没有拾取权"] = "没有拾取权",
+	font = "Fonts\\FRIZQT__.TTF"
+} end)
+
